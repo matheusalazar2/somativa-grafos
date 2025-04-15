cbe747dfa049bc39a0cfe65234f1e489be096c16
Salazar <matheusalazar@gmail.com>
2025-04-14 21:06:38 -0300

    feat: criar grafo de livros

---
+++ projectgrafos/src/Livro.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    @Override
+    public String toString() {
+        return titulo + " - " + autor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Livro)) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(titulo, livro.titulo) &&
+                Objects.equals(autor, livro.autor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(titulo, autor);
+    }
+}
